c0c08ebea164b1a34b3819daece77687d5feb2e9
Javier Colmenar <fjcolmenar@serbatic.es>
2022-08-16 17:50:01 +0200

    Initial commit

---
+++ src/main/java/com/openwebinars/SecondHandMarket/SecondHandMarketV02Application.java
@@ -0,0 +1,13 @@
+package com.openwebinars.SecondHandMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandMarketV02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondHandMarketV02Application.class, args);
+	}
+
+}
