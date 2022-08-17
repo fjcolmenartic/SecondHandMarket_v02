3d363e224433e38e9c49b593f752ab81a87586ad
Javier Colmenar <fjcolmenar@serbatic.es>
2022-08-17 08:31:37 +0200

    Adding Compra Entity.

---
+++ src/main/java/com/openwebinars/SecondHandMarket/modelo/Compra.java
@@ -0,0 +1,73 @@
+package com.openwebinars.SecondHandMarket.modelo;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Compra {
+
+    @Id @GeneratedValue
+    private long id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaCompra;
+
+    @ManyToOne
+    private Usuario propietario;
+
+    public Compra() {}
+
+    public Compra(Usuario propietario) {
+        this.propietario = propietario;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(Date fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public Usuario getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Usuario propietario) {
+        this.propietario = propietario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Compra compra = (Compra) o;
+        return id == compra.id && fechaCompra.equals(compra.fechaCompra) && propietario.equals(compra.propietario);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fechaCompra, propietario);
+    }
+
+    @Override
+    public String toString() {
+        return "Compra{" +
+                "id=" + id +
+                ", fechaCompra=" + fechaCompra +
+                ", propietario=" + propietario +
+                '}';
+    }
+}
