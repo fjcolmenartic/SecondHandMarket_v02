15472fd29271eac7736f35b07c2c151e31f8c24a
Javier Colmenar <fjcolmenar@serbatic.es>
2022-08-17 12:16:24 +0200

    Adding services for each entity with initial methods

---
+++ src/main/java/com/openwebinars/SecondHandMarket/servicios/CompraServicio.java
@@ -0,0 +1,43 @@
+package com.openwebinars.SecondHandMarket.servicios;
+
+import com.openwebinars.SecondHandMarket.modelo.Compra;
+import com.openwebinars.SecondHandMarket.modelo.Producto;
+import com.openwebinars.SecondHandMarket.modelo.Usuario;
+import com.openwebinars.SecondHandMarket.repositorios.CompraRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompraServicio {
+
+    @Autowired
+    CompraRepository repositorio;
+
+    @Autowired
+    ProductoServicio productoServicio;
+
+    public Compra insertar(Compra c, Usuario u) {
+        c.setPropietario(u);
+        return repositorio.save(c);
+    }
+
+    public Compra insertar(Compra c) {
+        return repositorio.save(c);
+    }
+
+    public Producto addProductoCompra(Producto p, Compra c) {
+        p.setCompra(c);
+        return productoServicio.editar(p);
+    }
+
+    public Compra buscarPorId(long id) {
+        return repositorio.findById(id).orElse(null);
+    }
+
+    public List<Compra> todas() {
+        return repositorio.findAll();
+    }
+
+}
