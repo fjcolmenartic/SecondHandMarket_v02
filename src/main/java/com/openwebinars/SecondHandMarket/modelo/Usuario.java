30fb79e53814fc76d2b3e446b48364cff6e67160
Javier Colmenar <fjcolmenar@serbatic.es>
2022-08-17 08:28:04 +0200

    Adding entity User

---
+++ src/main/java/com/openwebinars/SecondHandMarket/modelo/Usuario.java
@@ -0,0 +1,120 @@
+package com.openwebinars.SecondHandMarket.modelo;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String nombre;
+    private String apellidos;
+    private String avatar;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaAlta;
+
+    private String email;
+    private String password;
+
+    public Usuario() {}
+
+    public Usuario(long id, String nombre, String apellidos, String avatar, Date fechaAlta, String email, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.avatar = avatar;
+        this.fechaAlta = fechaAlta;
+        this.email = email;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id == usuario.id && nombre.equals(usuario.nombre) && apellidos.equals(usuario.apellidos) && avatar.equals(usuario.avatar) && fechaAlta.equals(usuario.fechaAlta) && email.equals(usuario.email) && password.equals(usuario.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, apellidos, avatar, fechaAlta, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", fechaAlta=" + fechaAlta +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
